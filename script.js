25cca86bd04b66b9350df127a04a2c41bed8c32d
Athrv0 <exposereality8@gmail.com>
2025-03-18 23:29:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// JavaScript is not needed for this specific animation, but here's a simple color change as an enhancement:
 window.onload = function() {
     let helloElement = document.getElementById('helloWorld');
-    let colors = ['#ff6a00', '#ee0979', '#a8ff78', '#42eaff'];
+    let colors = ['#ff8a00', '#00bcd4', '#ff4081', '#009688'];
 
     setInterval(() => {
         let randomColor = colors[Math.floor(Math.random() * colors.length)];
         helloElement.style.color = randomColor;
-    }, 1000);
+    }, 1500);
 };
